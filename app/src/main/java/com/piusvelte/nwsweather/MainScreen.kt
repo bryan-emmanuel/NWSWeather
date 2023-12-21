151a46a0fd87907a9bd8eb60eca22f65c356b430
Bryan Emmanuel <piusvelte@gmail.com>
2023-12-21 14:36:32 -0500

    Remove Points Screen

--- app/src/main/java/com/piusvelte/nwsweather/MainScreen.kt
+++ app/src/main/java/com/piusvelte/nwsweather/MainScreen.kt
@@ -3,17 +3,14 @@ package com.piusvelte.nwsweather
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
 import com.piusvelte.nwsweather.forecast.ForecastScreen
-import com.piusvelte.nwsweather.point.PointScreen
 
 @Composable
 fun MainScreen(
     modifier: Modifier = Modifier,
 ) {
     Column(modifier = modifier) {
-        PointScreen(modifier = modifier)
         ForecastScreen(modifier = modifier)
     }
 }
